19293132fda062be860136876198efd1648075ea
Miguel Angel <miguelapb35@gmail.com>
2020-04-08 15:57:21 -0500

    Updates

---
+++ precache-manifest.5d308ead0a78ab7a74cb26370334f998.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9a2240b67b818472a48170e780d336a",
+    "url": "/ReactPagination/index.html"
+  },
+  {
+    "revision": "a1e4c40c38d4a066902e",
+    "url": "/ReactPagination/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8fc1cb78926d6afadeea",
+    "url": "/ReactPagination/static/js/2.5a63eb93.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactPagination/static/js/2.5a63eb93.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1e4c40c38d4a066902e",
+    "url": "/ReactPagination/static/js/main.ceaf59ae.chunk.js"
+  },
+  {
+    "revision": "53604d43d82a67d09c3f",
+    "url": "/ReactPagination/static/js/runtime-main.d9b9f8f3.js"
+  }
+]);
